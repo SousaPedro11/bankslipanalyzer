0c80b72798658a55973c2b9a7120b6c109824fbe
Pedro Sousa <36779714+SousaPedro11@users.noreply.github.com>
2022-12-12 03:44:40 -0300

    feat: add nexxera regex

--- app/api/helpers/validators.py
+++ app/api/helpers/validators.py
@@ -43,3 +43,59 @@ def regex_barcode() -> str:
         "$",
     ]
     return "".join(regex_components)
+
+
+def regex_file_header_nexxera() -> str:
+    """
+    Regex to validate the file header from Nexxera
+    :return: str
+    """
+
+    regex_components = [
+        "^(?<banco>\\d{3})",  # Cod. do Banco na Compensacao
+        "(?<lote>0{4})",  # Lote de servico
+        "(?<registro>0)",  # Tipo de registro
+        "(?<filler>\\s{9})",  # Uso Exclusivo NEXXERA
+        "(?<inscricao_tipo>\\d)",  # Tipo de inscricao da empresa
+        "(?<inscricao_numero>\\d{14})",  # Numero de inscricao da empresa
+        "(?<convenio>.{20})",  # Codigo do convenio no banco
+        "(?<agencia_codigo>\\d{5})",  # Agencia mantenedora da conta
+        "(?<agencia_dv>.{1})",  # Digito verificador da agencia
+        "(?<conta_numero>\\d{12})",  # Numero da conta corrente
+        "(?<conta_dv>.{1})",  # Digito verificador da conta
+        "(?<ag_conta_dv>.{1})",  # Digito verificador da agencia/conta
+        "(?<empresa_nome>.{30})",  # Nome da empresa
+        "(?<banco_nome>.{30})",  # Nome do banco
+        "(?<van_nome>.{10})",  # Nome da VAN
+        "(?<arquivo_codigo>\\d)",  # Codigo remessa/retorno
+        "(?<arquivo_geracao_data>\\d{8})",  # Data de geracao do arquivo
+        "(?<arquivo_geracao_hora>\\d{6})",  # Hora de geracao do arquivo
+        "(?<arquivo_sequencia>\\d{7})",  # Numero sequencial do arquivo
+        "(?<arquivo_layout>020)",  # Numero da versao do layout do arquivo
+        "(?<arquivo_densidade>\\d{5})",  # Densidade de gravacao do arquivo
+        "(?<reservado_banco>.{19})",  # Uso exclusivo do banco
+        "(?<reservado_empresa>.{20})",  # Uso exclusivo da empresa
+        "(?<observacoes>.{29})",  # Observacao do layout bancario
+    ]
+
+    return "".join(regex_components)
+
+
+def regex_file_trailer_nexxera() -> str:
+    """
+    Regex to validate the file trailer from Nexxera
+    :return: str
+    """
+
+    regex_components = [
+        "^(?<banco>\\d{3})",  # Cod. do Banco na Compensacao
+        "(?<lote>9999)",  # Lote de servico
+        "(?<registro>9)",  # Tipo de registro
+        "(?<filler>\\s{9})",  # Uso Exclusivo NEXXERA
+        "(?<qtd_lotes>\\d{6})",  # Quantidade de lotes do arquivo
+        "(?<qtd_registros>\\d{6})",  # Quantidade de registros do arquivo
+        "(?<qtd_contas>\\d{6})",  # Quantidade de contas do arquivo
+        "(?<filler2>\\s{205})",  # Uso Exclusivo NEXXERA
+    ]
+
+    return "".join(regex_components)
